0b5db1a579ef82b3bcd7be65faa95acdbb3d4c14
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-08-16 04:38:37 +0545

    Add home and comment model as factory to locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,12 +1,17 @@
 import 'package:get_it/get_it.dart';
 
 import 'package:flutter_architecture/core/services/api.dart';
+import 'package:flutter_architecture/core/view_models/home_model.dart';
 import 'package:flutter_architecture/core/view_models/login_model.dart';
+import 'package:flutter_architecture/core/view_models/comments_model.dart';
 import 'package:flutter_architecture/core/services/authentication_service.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
+  locator.registerFactory(() => HomeModel());
+  locator.registerFactory(() => CommentsModel());
+
   locator.registerLazySingleton(() => Api());
   locator.registerLazySingleton(() => LoginModel());
   locator.registerLazySingleton(() => AuthenticationService());
