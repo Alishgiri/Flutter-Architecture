20fbc5dd42b69fd2d95ee1efed530306c0447d21
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-08-15 23:38:28 +0545

    Refactor login model

--- lib/core/view_models/login_model.dart
+++ lib/core/view_models/login_model.dart
@@ -1,25 +1,22 @@
-import 'package:flutter/material.dart';
-
 import 'package:flutter_architecture/locator.dart';
+import 'package:flutter_architecture/core/enum/view_state.dart';
+import 'package:flutter_architecture/core/view_models/base_model.dart';
 import 'package:flutter_architecture/core/services/authentication_service.dart';
 
-enum ViewState { Idle, Busy }
-
-class LoginModel extends ChangeNotifier {
+class LoginModel extends BaseModel {
   final AuthenticationService _authenticationService =
       locator<AuthenticationService>();
-  ViewState _state = ViewState.Idle;
 
-  ViewState get state => _state;
-
-  void setState(ViewState viewState) {
-    _state = viewState;
-    notifyListeners();
-  }
+  String errorMessage;
 
   Future<bool> login(String userIdText) async {
     setState(ViewState.Busy);
     final userId = int.tryParse(userIdText);
+    if (userId == null) {
+      errorMessage = "Value entered is not a number";
+      setState(ViewState.Idle);
+      return false;
+    }
     final success = await _authenticationService.login(userId);
     setState(ViewState.Idle);
     return success;
