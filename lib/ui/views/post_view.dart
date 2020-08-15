07c92ea96a58b4945875a6cff08db7bcb4eeca3c
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-08-16 04:40:23 +0545

    Complete post view ui

--- lib/ui/views/post_view.dart
+++ lib/ui/views/post_view.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flutter_architecture/core/models/post.dart';
+import 'package:flutter_architecture/core/models/user.dart';
+import 'package:flutter_architecture/ui/widgets/comments.dart';
 
 class PostView extends StatelessWidget {
+  final Post post;
+
+  PostView({this.post});
+
   @override
   Widget build(BuildContext context) {
+    final tt = Theme.of(context).textTheme;
+    final user = Provider.of<User>(context);
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: <Widget>[],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 10),
+            Text(post.title, style: tt.headline5),
+            Text('by ${user.username}', style: tt.caption),
+            const SizedBox(height: 10),
+            Text(post.body),
+            Comments(post.id),
+          ],
+        ),
       ),
     );
   }
