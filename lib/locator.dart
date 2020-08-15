7ea63dd7db97fcfad9449e9eab553dfcc3d3e78e
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-08-15 22:49:30 +0545

    Setup loctor to have lazy singleton

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,13 @@
 import 'package:get_it/get_it.dart';
 
+import 'package:flutter_architecture/core/services/api.dart';
+import 'package:flutter_architecture/core/view_models/login_model.dart';
+import 'package:flutter_architecture/core/services/authentication_service.dart';
+
 GetIt locator = GetIt.instance;
 
-void setupLocator() {}
+void setupLocator() {
+  locator.registerLazySingleton(() => Api());
+  locator.registerLazySingleton(() => LoginModel());
+  locator.registerLazySingleton(() => AuthenticationService());
+}
