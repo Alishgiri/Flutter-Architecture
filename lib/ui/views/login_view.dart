7099556691d2f1d826d8ab80a804dafebba40b67
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-08-15 22:52:04 +0545

    Create login view

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class LoginView extends StatelessWidget {
+import 'package:flutter_architecture/locator.dart';
+import 'package:flutter_architecture/ui/widgets/login_header.dart';
+import 'package:flutter_architecture/core/view_models/login_model.dart';
+
+class LoginView extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: <Widget>[],
+    return ChangeNotifierProvider<LoginModel>(
+      create: (context) => locator<LoginModel>(),
+      child: Consumer<LoginModel>(
+        builder: (context, model, child) => Scaffold(
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              LoginHeader(controller: _controller),
+              model.state == ViewState.Busy
+                  ? CircularProgressIndicator()
+                  : FlatButton(
+                      child: Text('Login'),
+                      onPressed: () async {
+                        final loginSuccess =
+                            await model.login(_controller.text);
+                        if (loginSuccess) {}
+                      },
+                    )
+            ],
+          ),
+        ),
       ),
     );
   }
