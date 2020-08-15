48d24eec792ecc465aa71dd9f2421c2ccec7c581
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-08-16 04:38:15 +0545

    Add user controller to Authentication Service

--- lib/core/services/authentication_service.dart
+++ lib/core/services/authentication_service.dart
@@ -1,11 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter_architecture/locator.dart';
+import 'package:flutter_architecture/core/models/user.dart';
 import 'package:flutter_architecture/core/services/api.dart';
 
 class AuthenticationService {
   Api _api = locator<Api>();
 
+  StreamController<User> userController = StreamController<User>();
+
   Future<bool> login(int userId) async {
     final fetchedUser = await _api.getUserProfile(userId);
-    return fetchedUser != null;
+    final hasUser = fetchedUser != null;
+    if (hasUser) userController.add(fetchedUser);
+    return hasUser;
   }
 }
