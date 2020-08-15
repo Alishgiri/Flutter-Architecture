439e2a8b01c08a72765d8027902011ed628de156
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-08-15 23:10:46 +0545

    Create Router class for navigation configuration

---
+++ lib/ui/router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_architecture/ui/views/home_view.dart';
+import 'package:flutter_architecture/ui/views/post_view.dart';
+import 'package:flutter_architecture/ui/views/login_view.dart';
+
+class Router {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/home':
+        return MaterialPageRoute(builder: (_) => HomeView());
+      case '/login':
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case '/post':
+        return MaterialPageRoute(builder: (_) => PostView());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined to ${settings.name}!'),
+            ),
+          ),
+        );
+    }
+  }
+}
